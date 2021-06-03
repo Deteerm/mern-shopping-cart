168cc899606f9a358693c4aafb3044f1ae82b59e
Determ <dominik.tarkiewicz@gmail.com>
2021-06-03 12:07:13 +0200

    another commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
-const PORT =
-  process.env.PORT ||
-  app.listen(PORT, () => console.log("Server running on port ${PORT}"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Server running on port ${PORT}"));
