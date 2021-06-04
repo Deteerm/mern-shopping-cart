e2d168611239a6fa4f64f2cd7e59056592ac2f8b
Determ <dominik.tarkiewicz@gmail.com>
2021-06-04 21:02:24 +0200

    Completed backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+const {
+  getAllProducts,
+  getProductById,
+} = require("../controller/productsController");
+
 //@desc   GET all products from db
 //@routes GET /api/products
 //@access Public
-router.get("/", (req, res) => {});
+router.get("/", getAllProducts);
 
-//@desc   GET a product from db
+//@desc   GET a product by id from db
 //@routes GET /api/products/:id
 //@access Public
-router.get("/:id", (req, res) => {});
+router.get("/:id", getProductById);
 
 module.exports = router;
