f439a4d5aea6d1b1b42f55860ae9545bb4b189c9
Determ <dominik.tarkiewicz@gmail.com>
2021-06-04 22:01:38 +0200

    Started work on frontend

---
+++ frontendd/src/screens/CartScreen.js
@@ -0,0 +1,7 @@
+import "./CartScreen.css";
+
+function CartScreen() {
+  return <div className="cartscreen">Cart Screen</div>;
+}
+
+export default CartScreen;
---
+++ frontendd/src/screens/HomeScreen.js
@@ -0,0 +1,7 @@
+import "./HomeScreen.css";
+
+function HomeScreen() {
+  return <div className="homescreen">Home Screen</div>;
+}
+
+export default HomeScreen;
